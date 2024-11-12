6630bf8093e2bba2a57fc89a7d68e20e313d1545
Raul Escobedo <rjescobedo@gmail.com>
2024-11-12 16:03:01 -0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
+function calculateTax(amount) {
+    const taxValue = amount * .10;
+    return taxValue;
+}
 
+function convertToUpperCase(text) {
+    const stringUpperCase = text.toUpperCase();
+    return stringUpperCase;
+}
 
+function findMaximum(num1, num2) {
+    if(num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
 
+function isPalindrome(word) {
+    const reversedWord = word.split('').reverse().join('');
+    if(word === reversedWord) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountedPrice = originalPrice - (originalPrice * (discountPercentage/100));
+    return discountedPrice;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
